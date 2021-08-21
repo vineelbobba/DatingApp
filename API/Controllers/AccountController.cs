995a49ad8a4a4b334417e1c02f492d1ffbee808e
vineelbobba <vineel.bobba@gmail.com>
2021-08-21 19:41:18 +0530

    End of section 5

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -45,7 +45,7 @@ namespace API.Controllers
             };
         }
 
-        [HttpPost]
+        [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
